0c06a51a19d1119da61b0ee631e1034d3af7b160
aiirohituzi <sinn5656@gmail.com>
2019-10-08 22:40:53 +0900

    메뉴의 전 버튼에 커스텀 버튼 컴포넌트 적용

--- Griffin-Warehouse-app/Menu.js
+++ Griffin-Warehouse-app/Menu.js
@@ -14,33 +14,34 @@ const Menu = () => {
     <View style={styles.container}>
       <StatusBar backgroundColor="blue" barStyle="dark-content" hidden="false" />
       <View style={styles.row}>
-        {/* <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.buttonValue}>{`버튼\n이름1`}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.buttonValue}>{`버튼\n이름2`}</Text>
-        </TouchableOpacity> */}
-        <CustomButton/>
+        <CustomButton
+            buttonColor={'#023e71'}
+            title={'데미지\n계산기'}
+            onPress={() => alert('데미지 계산기 버튼')}/>
         <CustomButton
             buttonColor={'#023e71'}
             title={'최대사속\n계산기'}
             onPress={() => alert('최대사속 계산기 버튼')}/>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.title}>{`필요 보석량\n계산기`}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.title}>{`필요 모의작전점수\n계산기`}</Text>
-        </TouchableOpacity>
+        <CustomButton
+            buttonColor={'#023e71'}
+            title={`필요 보석량\n계산기`}
+            onPress={() => alert('필요 보석량 계산기 버튼')}/>
+        <CustomButton
+            buttonColor={'#023e71'}
+            title={'필요 모의작전점수\n계산기'}
+            onPress={() => alert('필요 모의작전점수 계산기 버튼')}/>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.title}>{`경험치\n계산기`}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text sytle={styles.title}>{`작전능력\n계산기`}</Text>
-        </TouchableOpacity>
+        <CustomButton
+            buttonColor={'#023e71'}
+            title={'경험치\n계산기'}
+            onPress={() => alert('경험치 계산기 버튼')}/>
+        <CustomButton
+            buttonColor={'#023e71'}
+            title={'작전능력\n계산기'}
+            onPress={() => alert('작전능력 계산기 버튼')}/>
       </View>
     </View>
   );
