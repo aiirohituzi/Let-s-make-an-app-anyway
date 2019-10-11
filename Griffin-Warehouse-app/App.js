8189828f0acf17773b9ed5dde47c83dd943cc2f9
aiirohituzi <sinn5656@gmail.com>
2019-10-11 20:14:17 +0900

    DrawerNavigation으로 메뉴화면 구성을 변경

--- Griffin-Warehouse-app/App.js
+++ Griffin-Warehouse-app/App.js
@@ -1,156 +1,68 @@
 import React, { useState } from 'react';
 import {
-  View,
-  StyleSheet,
   StatusBar,
-  Modal,
-  Text,
-  Dimensions,
-  TouchableOpacity,
-  SafeAreaView
 } from 'react-native';
-import Constants from 'expo-constants';
-// import { createAppContainer } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-// import MenuScreen from './src/MenuScreen'
-import CustomButton from './src/CustomButton';
 import DamageCalc from './src/DamageCalc';
-import AgiCalc from './src/AgiCalc'
+import AgiCalc from './src/AgiCalc';
+// import Screen3 from './components/screen3';
+// import Screen1 from './components/screen4';
+// import Screen2 from './components/screen5';
+// import Screen3 from './components/screen5';
 
-const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [screen, setScreen] = useState(DamageCalc);
-  const [screenName, setScreenName] = useState('데미지 계산기');
-  
-  const modalOpen = (screenNum) => {
-    switch(screenNum) {
-      case 1:
-        setScreen(DamageCalc);
-        setScreenName('데미지 계산기');
-        break;
-      case 2:
-        setScreen(AgiCalc);
-        setScreenName('최대사속 계산기');
-        break;
-      default:
-    }
-    setModalVisible(true);
-  }
+import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" hidden="false" />
-
-      <View style={styles.navbar}>
-        <View style={styles.status}>
-          <Text style={{fontSize: 20, fontWeight: "600"}}>메인 화면</Text>
-        </View>
-      </View>
-
-      <View style={styles.row}>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={'데미지\n계산기'}
-          onPress={() => modalOpen(1)}/>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={'최대사속\n계산기'}
-          onPress={() => modalOpen(2)}/>
-      </View>
-      <View style={styles.row}>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={`필요 보석량\n계산기`}
-          onPress={() => alert('필요 보석량 계산기 버튼')}/>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={'필요 모의작전점수\n계산기'}
-          onPress={() => alert('필요 모의작전점수 계산기 버튼')}/>
-      </View>
-      <View style={styles.row}>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={'경험치\n계산기'}
-          onPress={() => alert('경험치 계산기 버튼')}/>
-        <CustomButton
-          buttonColor={'#023e71'}
-          title={'작전능력\n계산기'}
-          onPress={() => alert('작전능력 계산기 버튼')}/>
-      </View>
-
-      <Modal
-        animationType="none"
-        transparent={false}
-        visible={modalVisible}
-        onRequestClose={() => {
-          alert('Modal has been closed.');
-        }}>
-        <SafeAreaView style={{paddingTop: Constants.statusBarHeight}}>
-          <View style={styles.navbar}>
-            <TouchableOpacity style={styles.btnBack} onPress={() => setModalVisible(false)}>
-              <Text style={{fontSize: 18, fontWeight: "600"}}>뒤로</Text>
-            </TouchableOpacity>
-            <View style={styles.status}>
-              <Text style={{fontSize: 20, fontWeight: "600"}}>{ screenName }</Text>
-            </View>
-            <View style={{width: 50}} />
-          </View>
-          { screen }
-        </SafeAreaView>
-      </Modal>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    // padding: 8,
+const MyDrawerNavigator = createDrawerNavigator(
+  {
+    DamageCalc: {
+      screen: DamageCalc,
+      navigationOptions: {
+        drawerLabel: '데미지 계산기',
+      }
+    },
+    AgiCalc: {
+      screen: AgiCalc,
+      navigationOptions: {
+        drawerLabel: '사속 계산기',
+      }
+    },
+    // Screen3: {
+    //   screen: Screen3,
+    //   navigationOptions: {
+    //     drawerLabel: '화면3',
+    //   }
+    // },
+    // Screen4: {
+    //   screen: Screen4,
+    //   navigationOptions: {
+    //     drawerLabel: '화면4',
+    //   }
+    // },
+    // Screen5: {
+    //   screen: Screen5,
+    //   navigationOptions: {
+    //     drawerLabel: '화면5',
+    //   }
+    // },
+    // Screen6: {
+    //   screen: Screen6,
+    //   navigationOptions: {
+    //     drawerLabel: '화면6',
+    //   }
+    // }
   },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
+  {
+    contentOptions: {
+      activeTintColor: '#e91e63',
+    },
+    initialRouteName: 'DamageCalc',
+    hideStatusBar: true,
+    statusBarAnimation: false,
   },
-  navbar: {
-    width: Dimensions.get('window').width,
-    height: 50,
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: '#d6d7da',
-  },
-  status: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btnBack: {
-    width: 50,
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+  StatusBar.setBarStyle('dark-content')
+);
 
-// const AppNavigator = createStackNavigator(
-//   {
-//     Menu: MenuScreen,
-//     Main: Main,
-//   },
-//   {
-//     initialRouteName: 'Menu',
-//   }
-// );
-// const AppContainer = createAppContainer(AppNavigator);
-
-
-// const App = () => {
-//   return (
-//     <AppContainer />
-//   );
-// };
+const App = createAppContainer(MyDrawerNavigator);
 
 export default App;
