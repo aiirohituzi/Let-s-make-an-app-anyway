ea82d65db6f9433b28328a76eebcea0f45f7eab5
aiirohituzi <sinn5656@gmail.com>
2019-10-11 20:31:50 +0900

    바꾼 메뉴에 맞춰서 데미지계산기 스크린 변경

--- Griffin-Warehouse-app/src/DamageCalc.js
+++ Griffin-Warehouse-app/src/DamageCalc.js
@@ -4,74 +4,113 @@ import {
   Text,
   StyleSheet,
   Dimensions,
-  TextInput,
-  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput
 } from 'react-native';
 import Constants from 'expo-constants';
 
-const DamageCalc = () => {
-  let tdollAtk, tdollSkill, equipCritical;
+const DamageCalc = (props) => {
+  const [value1, setValue1] = useState('');
+  console.log(props)
   return (
-    <View style={styles.container}>
-      <View style={styles.flexRow}>
-        <Text style={styles.label}>인형 순수 화력 스탯 입력</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={text => tdollAtk = text}
-          value={tdollAtk}
-        />
-      </View>
-      
-      <View style={styles.flexRow}>
-        <Text style={styles.label}>인형 스킬 화력 배율(%) 입력</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={text => tdollSkill = text}
-          value={tdollSkill}
-        />
-      </View>
-      
-      <View style={styles.flexRow}>
-        <Text style={styles.label}>장비 치명상 입력</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={text => equipCritical = text}
-          value={equipCritical}
-        />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navbar}>
+        <TouchableOpacity style={styles.btnMenu} onPress={() => props.navigation.openDrawer()}>
+          <Text style={{fontSize: 18, fontWeight: "600"}}>메뉴</Text>
+        </TouchableOpacity>
+        <View style={styles.status}>
+          <Text style={{fontSize: 20, fontWeight: "600"}}>화면3</Text>
+        </View>
+        <View style={{width: 50}} />
       </View>
 
-      <Button
-        title="check"
-        onPress={() => alert(tdollAtk +'/'+ tdollSkill +'/'+ equipCritical)}
-      ></Button>
-    </View>
+      <View style={{padding: '5%'}}>
+        <View style={styles.flexRow}>
+          <View style={styles.inputLabelsView}>
+            <Text style={styles.inputLabels}>입력항목 1</Text>
+          </View>
+          <TextInput
+            style={styles.inputs}
+            onChangeText={text => setValue1(text)}
+            value={value1}
+            placeholder="입력창 1"
+          />
+        </View>
+        <Text style={{textAlign: 'right', marginTop: 5, marginBottom: 5}}>입력값 state 변화 확인용 : {value1}</Text>
+      </View>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 50 - Constants.statusBarHeight,
+    // width: Dimensions.get('window').width,
+    // height: Dimensions.get('window').height,
     padding: 10,
+    flex: 1,
+    // justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
   flexRow: {
     flexDirection: 'row',
-    height: 30,
-    width: '100%',
   },
-  label: {
-    flex: 3,
+  navbar: {
+    width: Dimensions.get('window').width,
+    height: 50,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#d6d7da',
+  },
+  btnMenu: {
+    width: 50,
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    borderColor: 'gray',
-    borderWidth: 1,
   },
-  input: {
-    flex: 2,
-    height: 30,
-    borderColor: 'gray',
+  status: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputLabelsView: {
+    flex: 3,
+    height: 40,
+
+    borderColor: '#bbb',
     borderWidth: 1,
+    borderRightWidth: 0,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    backgroundColor: '#bbb',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  inputLabels: {
+    // flex: 3,
+    // height: 40,
+    // lineHeight: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+    textAlign: 'right',
+
+    // borderColor: 'gray',
+    // borderWidth: 1,
+    // borderTopLeftRadius: 5,
+    // borderBottomLeftRadius: 5,
+  },
+  inputs: {
+    flex: 2,
+    textAlign: 'right',
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
   }
 });
 
