7253cd59e96e5a3cbb7c908ece0b7f3a4ce3e268
aiirohituzi <sinn5656@gmail.com>
2019-10-23 22:06:35 +0900

    기본적인 영역의 UI 배치

--- Griffin-Warehouse-app/src/AgiCalc.js
+++ Griffin-Warehouse-app/src/AgiCalc.js
@@ -7,11 +7,21 @@ import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 import Constants from 'expo-constants';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const AgiCalc = (props) => {
+  const [buffer1Buff, setBuffer1Buff] = useState('');
+  const [buffer2Buff, setBuffer2Buff] = useState('');
+  const [buffer3Buff, setBuffer3Buff] = useState('');
+  const [buffer4Buff, setBuffer4Buff] = useState('');
+  const [buffer1Skill, setBuffer1Skill] = useState('');
+  const [buffer2Skill, setBuffer2Skill] = useState('');
+  const [buffer3Skill, setBuffer3Skill] = useState('');
+  const [buffer4Skill, setBuffer4Skill] = useState('');
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
@@ -23,6 +33,154 @@ const AgiCalc = (props) => {
             <Text style={{fontSize: 20, fontWeight: "600"}}>최대사속 계산기</Text>
           </View>
           <View style={{width: 50}} />
+        </View>
+
+        <View style={{padding: '5%'}}>
+          <View style={styles.flexRow}>
+            <View style={[styles.inputLabelsView, {flex: 3}]}>
+              <Text style={styles.inputLabels}>인형 순수 화력 스탯 입력</Text>
+            </View>
+            <TextInput
+              style={[styles.inputs, {flex: 2}]}
+              placeholder="화력(호감도포함)"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.flexRow}>
+            <View style={[styles.inputLabelsView, {flex: 3}]}>
+              <Text style={styles.inputLabels}>인형 순수 화력 스탯 입력</Text>
+            </View>
+            <TextInput
+              style={[styles.inputs, {flex: 2}]}
+              placeholder="화력(호감도포함)"
+              keyboardType="numeric"
+            />
+          </View>
+
+          
+          <View style={[styles.baseLabelsView, styles.radiusTitle]}>
+            <Text style={styles.baseLabelsAlignCenter}>화력 버퍼 정보 입력</Text>
+          </View>
+          <View style={styles.flexRowNoMargin}>
+            <View style={[styles.baseLabelsView, {flex: 1}]}>
+              <Text style={styles.baseLabelsAlignCenter}>버퍼 인형 선택</Text>
+            </View>
+            <View style={[styles.baseLabelsView, {flex: 1}]}>
+              <Text style={styles.baseLabelsAlignCenter}>화력 진형버프</Text>
+            </View>
+            <View style={[styles.baseLabelsView, {flex: 1}]}>
+              <Text style={styles.baseLabelsAlignCenter}>버프 스킬 배율</Text>
+            </View>
+          </View>
+          <View style={styles.flexRowNoMargin}>
+            <View
+              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
+              <Text style={{ textAlign: 'right' }}>
+                버퍼 인형 1
+              </Text>
+            </View>
+            <TextInput
+              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer1Buff(text)}
+              value={buffer1Buff}
+              placeholder="인형1 진형버프"
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer1Skill(text)}
+              value={buffer1Skill}
+              placeholder="인형1 스킬배율"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.flexRowNoMargin}>
+            <View
+              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
+              <Text style={{ textAlign: 'right' }}>
+                버퍼 인형 2
+              </Text>
+            </View>
+            <TextInput
+              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer2Buff(text)}
+              value={buffer2Buff}
+              placeholder="인형2 진형버프"
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer2Skill(text)}
+              value={buffer2Skill}
+              placeholder="인형2 스킬배율"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.flexRowNoMargin}>
+            <View
+              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
+              <Text style={{ textAlign: 'right' }}>
+                버퍼 인형 3
+              </Text>
+            </View>
+            <TextInput
+              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer3Buff(text)}
+              value={buffer3Buff}
+              placeholder="인형3 진형버프"
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer3Skill(text)}
+              value={buffer3Skill}
+              placeholder="인형3 스킬배율"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.flexRow}>
+            <View
+              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
+              <Text style={{ textAlign: 'right' }}>
+                버퍼 인형 4
+              </Text>
+            </View>
+            <TextInput
+              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              onChangeText={text => setBuffer4Buff(text)}
+              value={buffer4Buff}
+              placeholder="인형4 진형버프"
+              keyboardType="numeric"
+            />
+            <TextInput
+              style={[styles.inputs, {flex: 1, borderTopWidth: 0, borderTopRightRadius: 0}]}
+              onChangeText={text => setBuffer4Skill(text)}
+              value={buffer4Skill}
+              placeholder="인형4 스킬배율"
+              keyboardType="numeric"
+            />
+          </View>
+
+          <View style={styles.flexRow}>
+            <View style={[styles.inputLabelsView, {flex: 3}]}>
+              <Text style={styles.inputLabels}>인형 순수 화력 스탯 입력</Text>
+            </View>
+            <TextInput
+              style={[styles.inputs, {flex: 2}]}
+              placeholder="화력(호감도포함)"
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={styles.flexRow}>
+            <View style={[styles.inputLabelsView, {flex: 3}]}>
+              <Text style={styles.inputLabels}>인형 순수 화력 스탯 입력</Text>
+            </View>
+            <TextInput
+              style={[styles.inputs, {flex: 2}]}
+              placeholder="화력(호감도포함)"
+              keyboardType="numeric"
+            />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -65,6 +223,108 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  
+  inputLabelsView: {
+    // flex: 3,
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    backgroundColor: '#bbb',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  inputLabels: {
+    // flex: 3,
+    // height: 40,
+    // lineHeight: 40,
+    textAlign: 'right',
+
+    // borderColor: 'gray',
+    // borderWidth: 1,
+    // borderTopLeftRadius: 5,
+    // borderBottomLeftRadius: 5,
+  },
+  inputs: {
+    // flex: 2,
+    textAlign: 'right',
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+
+  
+  baseLabelsView: {
+    // flex: 3,
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+
+    borderColor: '#bbb',
+    borderWidth: 1,
+    backgroundColor: '#bbb',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  resultLabelsView: {
+    // flex: 3,
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+
+    borderColor: '#bbb',
+    borderWidth: 1,
+    backgroundColor: '#ddd',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  baseLabelsAlignCenter: {
+    // flex: 3,
+    // height: 40,
+    // lineHeight: 40,
+    textAlign: 'center',
+
+    // borderColor: 'gray',
+    // borderWidth: 1,
+    // borderTopLeftRadius: 5,
+    // borderBottomLeftRadius: 5,
+  },
+  baseLabelsAlignRight: {
+    // flex: 3,
+    // height: 40,
+    // lineHeight: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+    textAlign: 'right',
+
+    // borderColor: 'gray',
+    // borderWidth: 1,
+    // borderTopLeftRadius: 5,
+    // borderBottomLeftRadius: 5,
+  },
+  inputsMiddle: {
+    flex: 2,
+    textAlign: 'right',
+    height: 40,
+    paddingRight: 5,
+    paddingLeft: 5,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderRightWidth: 0,
+  },
+  radiusTitle: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
   },
 });
 
