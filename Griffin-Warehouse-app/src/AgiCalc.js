b7b556f9e70dd385f333d8c28f56e6c5c9f97e3f
aiirohituzi <sinn5656@gmail.com>
2019-10-25 23:31:18 +0900

    - 사속 계산기 스킬 배율 입력방식을 결정하는 버튼 UI 추가 - prettier 설치 및 설정파일 추가

--- Griffin-Warehouse-app/src/AgiCalc.js
+++ Griffin-Warehouse-app/src/AgiCalc.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -8,86 +8,143 @@ import {
   ScrollView,
   TouchableOpacity,
   TextInput,
-} from 'react-native';
-import Constants from 'expo-constants';
-import Icon from 'react-native-vector-icons/Ionicons';
+  Switch,
+} from "react-native";
+import Constants from "expo-constants";
+import Icon from "react-native-vector-icons/Ionicons";
 
-const AgiCalc = (props) => {
-  const [buffer1Buff, setBuffer1Buff] = useState('');
-  const [buffer2Buff, setBuffer2Buff] = useState('');
-  const [buffer3Buff, setBuffer3Buff] = useState('');
-  const [buffer4Buff, setBuffer4Buff] = useState('');
-  const [buffer1Skill, setBuffer1Skill] = useState('');
-  const [buffer2Skill, setBuffer2Skill] = useState('');
-  const [buffer3Skill, setBuffer3Skill] = useState('');
-  const [buffer4Skill, setBuffer4Skill] = useState('');
+const AgiCalc = props => {
+  const [buffer1Buff, setBuffer1Buff] = useState("");
+  const [buffer2Buff, setBuffer2Buff] = useState("");
+  const [buffer3Buff, setBuffer3Buff] = useState("");
+  const [buffer4Buff, setBuffer4Buff] = useState("");
+  const [buffer1Skill, setBuffer1Skill] = useState("");
+  const [buffer2Skill, setBuffer2Skill] = useState("");
+  const [buffer3Skill, setBuffer3Skill] = useState("");
+  const [buffer4Skill, setBuffer4Skill] = useState("");
+
+  const [inputFlag, setInputFlag] = useState(false);
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.navbar}>
-          <TouchableOpacity style={styles.btnMenu} onPress={() => props.navigation.openDrawer()}>
-            <Text style={{fontSize: 18, fontWeight: "600"}}><Icon name="ios-menu" size={30} color="#555" /></Text>
+          <TouchableOpacity
+            style={styles.btnMenu}
+            onPress={() => props.navigation.openDrawer()}
+          >
+            <Text style={{ fontSize: 18, fontWeight: "600" }}>
+              <Icon name="ios-menu" size={30} color="#555" />
+            </Text>
           </TouchableOpacity>
           <View style={styles.status}>
-            <Text style={{fontSize: 20, fontWeight: "600"}}>최대사속 계산기</Text>
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              최대사속 계산기
+            </Text>
           </View>
-          <View style={{width: 50}} />
+          <View style={{ width: 50 }} />
         </View>
 
-        <View style={{padding: '5%'}}>
+        <View style={{ padding: "5%" }}>
           <View style={styles.flexRow}>
-            <View style={[styles.inputLabelsView, {flex: 3}]}>
+            <View style={[styles.inputLabelsView, { flex: 3 }]}>
               <Text style={styles.inputLabels}>인형 사속 입력</Text>
             </View>
             <TextInput
-              style={[styles.inputs, {flex: 2}]}
+              style={[styles.inputs, { flex: 2 }]}
               placeholder="사속"
               keyboardType="numeric"
             />
           </View>
           <View style={styles.flexRow}>
-            <View style={[styles.inputLabelsView, {flex: 3}]}>
-              <Text style={styles.inputLabels}>인형 사속 스킬 배율 입력(%)</Text>
+            <TouchableOpacity
+              style={[
+                {
+                  flex: 1,
+                  justifyContent: "center",
+                  height: 40,
+                  borderWidth: 1,
+                  borderTopLeftRadius: 5,
+                  borderBottomLeftRadius: 5,
+                },
+                inputFlag ? {} : { backgroundColor: "#ddd" },
+              ]}
+              onPress={() => setInputFlag(false)}
+            >
+              <Text style={{ textAlign: "center" }}>스킬 사속 직접 입력</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[
+                {
+                  flex: 1,
+                  justifyContent: "center",
+                  height: 40,
+                  borderWidth: 1,
+                  borderLeftWidth: 0,
+                  borderTopRightRadius: 5,
+                  borderBottomRightRadius: 5,
+                },
+                inputFlag ? { backgroundColor: "#ddd" } : {},
+              ]}
+              onPress={() => setInputFlag(true)}
+            >
+              <Text style={{ textAlign: "center" }}>목록에서 선택</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.flexRow}>
+            <View style={[styles.inputLabelsView, { flex: 3 }]}>
+              <Text style={styles.inputLabels}>
+                인형 사속 스킬 배율 입력(%)
+              </Text>
             </View>
             <TextInput
-              style={[styles.inputs, {flex: 2}]}
+              style={[styles.inputs, { flex: 2 }]}
               placeholder="스킬 배율(%)"
               keyboardType="numeric"
             />
           </View>
-
-          
           <View style={[styles.baseLabelsView, styles.radiusTitle]}>
-            <Text style={styles.baseLabelsAlignCenter}>사속 버퍼 정보 입력</Text>
+            <Text style={styles.baseLabelsAlignCenter}>
+              사속 버퍼 정보 입력
+            </Text>
           </View>
           <View style={styles.flexRowNoMargin}>
-            <View style={[styles.baseLabelsView, {flex: 1}]}>
+            <View style={[styles.baseLabelsView, { flex: 1 }]}>
               <Text style={styles.baseLabelsAlignCenter}>버퍼 인형 선택</Text>
             </View>
-            <View style={[styles.baseLabelsView, {flex: 1}]}>
+            <View style={[styles.baseLabelsView, { flex: 1 }]}>
               <Text style={styles.baseLabelsAlignCenter}>사속 진형버프</Text>
             </View>
-            <View style={[styles.baseLabelsView, {flex: 1}]}>
+            <View style={[styles.baseLabelsView, { flex: 1 }]}>
               <Text style={styles.baseLabelsAlignCenter}>버프 스킬 배율</Text>
             </View>
           </View>
           <View style={styles.flexRowNoMargin}>
             <View
-              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
-              <Text style={{ textAlign: 'center' }}>
-                버퍼 인형 1
-              </Text>
+              style={[
+                styles.baseLabelsView,
+                { flex: 1, borderTopWidth: 0, borderRightWidth: 0 },
+              ]}
+            >
+              <Text style={{ textAlign: "center" }}>버퍼 인형 1</Text>
             </View>
             <TextInput
-              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              style={[styles.inputsMiddle, { flex: 1, borderTopWidth: 0 }]}
               onChangeText={text => setBuffer1Buff(text)}
               value={buffer1Buff}
               placeholder="인형1 진형버프"
               keyboardType="numeric"
             />
             <TextInput
-              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              style={[
+                styles.inputs,
+                {
+                  flex: 1,
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 0,
+                  borderTopWidth: 0,
+                },
+              ]}
               onChangeText={text => setBuffer1Skill(text)}
               value={buffer1Skill}
               placeholder="인형1 스킬배율"
@@ -96,20 +153,30 @@ const AgiCalc = (props) => {
           </View>
           <View style={styles.flexRowNoMargin}>
             <View
-              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
-              <Text style={{ textAlign: 'center' }}>
-                버퍼 인형 2
-              </Text>
+              style={[
+                styles.baseLabelsView,
+                { flex: 1, borderTopWidth: 0, borderRightWidth: 0 },
+              ]}
+            >
+              <Text style={{ textAlign: "center" }}>버퍼 인형 2</Text>
             </View>
             <TextInput
-              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              style={[styles.inputsMiddle, { flex: 1, borderTopWidth: 0 }]}
               onChangeText={text => setBuffer2Buff(text)}
               value={buffer2Buff}
               placeholder="인형2 진형버프"
               keyboardType="numeric"
             />
             <TextInput
-              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              style={[
+                styles.inputs,
+                {
+                  flex: 1,
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 0,
+                  borderTopWidth: 0,
+                },
+              ]}
               onChangeText={text => setBuffer2Skill(text)}
               value={buffer2Skill}
               placeholder="인형2 스킬배율"
@@ -118,20 +185,30 @@ const AgiCalc = (props) => {
           </View>
           <View style={styles.flexRowNoMargin}>
             <View
-              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
-              <Text style={{ textAlign: 'center' }}>
-                버퍼 인형 3
-              </Text>
+              style={[
+                styles.baseLabelsView,
+                { flex: 1, borderTopWidth: 0, borderRightWidth: 0 },
+              ]}
+            >
+              <Text style={{ textAlign: "center" }}>버퍼 인형 3</Text>
             </View>
             <TextInput
-              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              style={[styles.inputsMiddle, { flex: 1, borderTopWidth: 0 }]}
               onChangeText={text => setBuffer3Buff(text)}
               value={buffer3Buff}
               placeholder="인형3 진형버프"
               keyboardType="numeric"
             />
             <TextInput
-              style={[styles.inputs, {flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderTopWidth: 0}]}
+              style={[
+                styles.inputs,
+                {
+                  flex: 1,
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 0,
+                  borderTopWidth: 0,
+                },
+              ]}
               onChangeText={text => setBuffer3Skill(text)}
               value={buffer3Skill}
               placeholder="인형3 스킬배율"
@@ -140,20 +217,25 @@ const AgiCalc = (props) => {
           </View>
           <View style={styles.flexRow}>
             <View
-              style={[styles.baseLabelsView, {flex: 1, borderTopWidth: 0, borderRightWidth: 0}]}>
-              <Text style={{ textAlign: 'center' }}>
-                버퍼 인형 4
-              </Text>
+              style={[
+                styles.baseLabelsView,
+                { flex: 1, borderTopWidth: 0, borderRightWidth: 0 },
+              ]}
+            >
+              <Text style={{ textAlign: "center" }}>버퍼 인형 4</Text>
             </View>
             <TextInput
-              style={[styles.inputsMiddle, {flex: 1, borderTopWidth: 0}]}
+              style={[styles.inputsMiddle, { flex: 1, borderTopWidth: 0 }]}
               onChangeText={text => setBuffer4Buff(text)}
               value={buffer4Buff}
               placeholder="인형4 진형버프"
               keyboardType="numeric"
             />
             <TextInput
-              style={[styles.inputs, {flex: 1, borderTopWidth: 0, borderTopRightRadius: 0}]}
+              style={[
+                styles.inputs,
+                { flex: 1, borderTopWidth: 0, borderTopRightRadius: 0 },
+              ]}
               onChangeText={text => setBuffer4Skill(text)}
               value={buffer4Skill}
               placeholder="인형4 스킬배율"
@@ -162,23 +244,19 @@ const AgiCalc = (props) => {
           </View>
 
           <View style={styles.flexRow}>
-            <View style={[styles.inputLabelsView, {flex: 3}]}>
+            <View style={[styles.inputLabelsView, { flex: 3 }]}>
               <Text style={styles.inputLabels}>필요 버프량</Text>
             </View>
-            <View style={[styles.valueLables, {flex: 2}]}>
-              <Text style={{ textAlign: 'right' }}>
-                0                  
-              </Text>
+            <View style={[styles.valueLables, { flex: 2 }]}>
+              <Text style={{ textAlign: "right" }}>0</Text>
             </View>
           </View>
           <View style={styles.flexRow}>
-            <View style={[styles.inputLabelsView, {flex: 3}]}>
+            <View style={[styles.inputLabelsView, { flex: 3 }]}>
               <Text style={styles.inputLabels}>자버프 사용 후 필요 버프량</Text>
             </View>
-            <View style={[styles.valueLables, {flex: 2}]}>
-              <Text style={{ textAlign: 'right' }}>
-                0                  
-              </Text>
+            <View style={[styles.valueLables, { flex: 2 }]}>
+              <Text style={{ textAlign: "right" }}>0</Text>
             </View>
           </View>
         </View>
@@ -186,7 +264,6 @@ const AgiCalc = (props) => {
     </SafeAreaView>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -196,55 +273,55 @@ const styles = StyleSheet.create({
     flex: 1,
     // justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
   },
   flexRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 5,
   },
   flexRowNoMargin: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   navbar: {
-    width: Dimensions.get('window').width,
+    width: Dimensions.get("window").width,
     height: 50,
-    flexDirection: 'row',
+    flexDirection: "row",
     borderBottomWidth: 1,
-    borderBottomColor: '#d6d7da',
+    borderBottomColor: "#d6d7da",
   },
   btnMenu: {
     width: 50,
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   status: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  
+
   inputLabelsView: {
     // flex: 3,
     height: 40,
     paddingRight: 5,
     paddingLeft: 5,
 
-    borderColor: '#bbb',
+    borderColor: "#bbb",
     borderWidth: 1,
     borderRightWidth: 0,
     borderTopLeftRadius: 5,
     borderBottomLeftRadius: 5,
-    backgroundColor: '#bbb',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: "#bbb",
+    alignItems: "stretch",
+    justifyContent: "center",
   },
   inputLabels: {
     // flex: 3,
     // height: 40,
     // lineHeight: 40,
-    textAlign: 'right',
+    textAlign: "right",
 
     // borderColor: 'gray',
     // borderWidth: 1,
@@ -253,28 +330,27 @@ const styles = StyleSheet.create({
   },
   inputs: {
     // flex: 2,
-    textAlign: 'right',
+    textAlign: "right",
     height: 40,
     paddingRight: 5,
     paddingLeft: 5,
-    borderColor: '#bbb',
+    borderColor: "#bbb",
     borderWidth: 1,
     borderTopRightRadius: 5,
     borderBottomRightRadius: 5,
   },
 
-  
   baseLabelsView: {
     // flex: 3,
     height: 40,
     paddingRight: 5,
     paddingLeft: 5,
 
-    borderColor: '#bbb',
+    borderColor: "#bbb",
     borderWidth: 1,
-    backgroundColor: '#bbb',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: "#bbb",
+    alignItems: "stretch",
+    justifyContent: "center",
   },
   resultLabelsView: {
     // flex: 3,
@@ -282,17 +358,17 @@ const styles = StyleSheet.create({
     paddingRight: 5,
     paddingLeft: 5,
 
-    borderColor: '#bbb',
+    borderColor: "#bbb",
     borderWidth: 1,
-    backgroundColor: '#ddd',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: "#ddd",
+    alignItems: "stretch",
+    justifyContent: "center",
   },
   baseLabelsAlignCenter: {
     // flex: 3,
     // height: 40,
     // lineHeight: 40,
-    textAlign: 'center',
+    textAlign: "center",
 
     // borderColor: 'gray',
     // borderWidth: 1,
@@ -305,7 +381,7 @@ const styles = StyleSheet.create({
     // lineHeight: 40,
     paddingRight: 5,
     paddingLeft: 5,
-    textAlign: 'right',
+    textAlign: "right",
 
     // borderColor: 'gray',
     // borderWidth: 1,
@@ -314,11 +390,11 @@ const styles = StyleSheet.create({
   },
   inputsMiddle: {
     flex: 2,
-    textAlign: 'right',
+    textAlign: "right",
     height: 40,
     paddingRight: 5,
     paddingLeft: 5,
-    borderColor: '#bbb',
+    borderColor: "#bbb",
     borderWidth: 1,
     borderRightWidth: 0,
   },
@@ -331,12 +407,12 @@ const styles = StyleSheet.create({
     paddingRight: 5,
     paddingLeft: 5,
     borderWidth: 1,
-    borderColor: '#bbb',
-    backgroundColor: '#ddd',
+    borderColor: "#bbb",
+    backgroundColor: "#ddd",
     borderTopRightRadius: 5,
     borderBottomRightRadius: 5,
-    justifyContent: 'center',
-  }
+    justifyContent: "center",
+  },
 });
 
 export default AgiCalc;
