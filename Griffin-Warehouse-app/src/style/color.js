0fc9938e18c42f40f61ad779c71e7272e84d7898
aiirohituzi <sinn5656@gmail.com>
2019-10-27 23:18:08 +0900

    주요 컬러 정보들 통합해서 정리

---
+++ Griffin-Warehouse-app/src/style/color.js
@@ -0,0 +1,6 @@
+export var BORDER_COLOR = "#d4cdbd";
+export var BORDER_SHADOW = "#d4cdbd";
+export var ADDON_LABEL = "#d4cdbd";
+export var OUTPUT_LABEL = "#eeebe4";
+export var TAB_ACTIVE = "#c3b9a2";
+export var TAB_INACTIVE = "#d4cdbd";
